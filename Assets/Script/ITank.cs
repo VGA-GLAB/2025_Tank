fc1c6a521d1a85aa5c311815be35a4b0a0c10dbe
tomo-1230 <satokawa1130tomoki2@gmail.com>
2025-10-05 16:45:34 +0900

    インターフェース

---
+++ Assets/Script/ITank.cs
@@ -0,0 +1,9 @@
+﻿public interface ITank
+{
+    public int Hp { get; }
+    public int ATK { get; }
+    public int MoveSpeed { get; }
+    public int BulletInterval { get; }
+    public void Hit(int atk);
+    public void Die();
+}
