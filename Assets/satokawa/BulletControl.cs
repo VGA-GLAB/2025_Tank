8a50f9a1d03bf54bd5105247b6c1f2635bd1b787
tomo-1230 <satokawa1130tomoki2@gmail.com>
2025-10-05 17:47:20 +0900

    ゲームマネージャーと弾コントロール

---
+++ Assets/satokawa/BulletControl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+[RequireComponent (typeof(Rigidbody))]
+public class BulletControl : MonoBehaviour
+{
+    [SerializeField] private float _bulletSpeed;//弾が進むスピード
+    [SerializeField] public int _atk;//攻撃力　クローンする時に入れる
+    private Rigidbody rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+     rb = GetComponent<Rigidbody>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(this.transform.forward * _bulletSpeed * Time.deltaTime,ForceMode.Impulse);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        ITank tank = collision.gameObject.GetComponent<ITank>();
+        if (tank != null)
+            tank.Hit(_atk);
+    }
+
+}
---
+++ Assets/satokawa/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private ITank player; //プレイヤー　あとでプレイヤークラスに変える
+    [SerializeField] private ITank[] enemys; //敵　あとで書き換え
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.Hp <= 0)
+            GameOver();
+
+        bool isTankActive = false;
+        foreach (ITank tank in enemys)
+        {
+            //Move処理
+
+            if (tank != null && tank.Hp > 0)
+            { 
+                isTankActive =true;
+            }
+        }
+        if (!isTankActive)
+        {
+            GameClear();
+        }
+    }
+    /// <summary>
+    /// ゲームオーバー処理
+    /// </summary>
+    private void GameOver()
+    {
+        Debug.Log("ゲームオーバー");
+    }
+    /// <summary>
+    /// ゲームクリア処理
+    /// </summary>
+    private void GameClear()
+    {
+        Debug.Log("ゲームクリア");
+    }
+}
