119732f5b5363ab061a83d6415edbc8f9e15af7e
YamamiyaShota <153970961+YamamiyaShota@users.noreply.github.com>
2025-10-05 17:41:42 +0900

    PlayerControllerの作成

---
+++ Assets/yamamiya/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour, ITank
+{
+    [SerializeField] private int _hp;
+    [SerializeField] private int _atk;
+    [SerializeField] private int _moveSpeed;
+    [SerializeField] private int _bulletInterval;
+
+    public int Hp => _hp;
+    public int ATK => _atk;
+    public int MoveSpeed => _moveSpeed;
+    public int BulletInterval => _bulletInterval;
+
+    public void Die()
+    {
+
+    }
+
+    public void Hit(int atk)
+    {
+
+    }
+}
