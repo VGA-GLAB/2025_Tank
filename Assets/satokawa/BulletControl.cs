3160b5288f3904b8c0bf34d08d5d6adf932a5690
tomo-1230 <satokawa1130tomoki2@gmail.com>
2025-10-06 17:59:55 +0900

    [fix]弾の動きを修正

--- Assets/satokawa/BulletControl.cs
+++ Assets/satokawa/BulletControl.cs
@@ -1,20 +1,26 @@
 ﻿using UnityEngine;
-[RequireComponent (typeof(Rigidbody))]
+[RequireComponent(typeof(Rigidbody))]
 public class BulletControl : MonoBehaviour
 {
     [SerializeField] private float _bulletSpeed;//弾が進むスピード
     [SerializeField] public int _atk;//攻撃力　クローンする時に入れる
+    [SerializeField] private float destroyDistance;
     private Rigidbody rb;
+    private Vector3 startPosition;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-     rb = GetComponent<Rigidbody>();   
+        rb = GetComponent<Rigidbody>();
+        startPosition = this.transform.position;
     }
-
     // Update is called once per frame
     void Update()
     {
-        rb.AddForce(this.transform.forward * _bulletSpeed * Time.deltaTime,ForceMode.Impulse);
+        rb.linearVelocity = this.transform.forward * _bulletSpeed;
+        if (Vector3.Distance(this.transform.position, startPosition) > destroyDistance)
+        {
+            Destroy(this.gameObject);
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -22,6 +28,8 @@ public class BulletControl : MonoBehaviour
         ITank tank = collision.gameObject.GetComponent<ITank>();
         if (tank != null)
             tank.Hit(_atk);
+
+        Destroy(this.gameObject);
     }
 
 }
