a243519e4de78d21b6fc0d24d8ec93f8b6c83927
tomo-1230 <satokawa1130tomoki2@gmail.com>
2025-10-08 00:20:31 +0900

    [upgrade]プレイヤーの生成場所を指定できるように変更

--- Assets/satokawa/Photon/NetworkManager.cs
+++ Assets/satokawa/Photon/NetworkManager.cs
@@ -2,15 +2,24 @@
 using Photon.Pun;
 using Photon.Realtime;
 /// <summary>
-/// PhotonのNetworkを管理
+/// ネットワーク関係を管理
 /// </summary>
 public class NetworkManager : MonoBehaviourPunCallbacks
 {
-    [SerializeField,Header("プレイヤーPrefab")] private GameObject tankPrefab;
+    [SerializeField, Header("プレイヤーPrefab    !!Resourcesフォルダに入れる!!")] private GameObject _tankPrefab;
+    [SerializeField, Header("プレイヤーの生成位置")] private Transform[] _clonePosition;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        PhotonNetwork.ConnectUsingSettings();
+        //Titleで接続した場合はそのまま生成し未接続の場合は接続する
+        if (PhotonNetwork.IsConnected)
+        {
+            CreatePlayerTank();
+        }
+        else
+        {
+            PhotonNetwork.ConnectUsingSettings();
+        }
     }
 
     // Update is called once per frame
@@ -22,12 +31,29 @@ public class NetworkManager : MonoBehaviourPunCallbacks
     {
         PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions(), TypedLobby.Default);
     }
-    /// <summary>
-    /// プレイヤー生成処理
-    /// </summary>
     public override void OnJoinedRoom()
     {
-        Vector3 position = new Vector3(Random.Range(-3f, 3f), 3f, Random.Range(-3f, 3f));
-        PhotonNetwork.Instantiate("Tank", position, Quaternion.identity);
+        CreatePlayerTank();
+    }
+    /// <summary>
+    /// プレイヤーを生成
+    /// </summary>
+    private void CreatePlayerTank()
+    {
+        int playerCount = PhotonNetwork.CurrentRoom.PlayerCount;
+        Vector3 position;
+
+        //プレイヤーの数が_clonePositionを越えていないかを確認
+        if (playerCount < _clonePosition.Length)
+        {
+            position = _clonePosition[PhotonNetwork.CurrentRoom.PlayerCount - 1].position;
+        }
+        //超えていたらランダムな場所にする
+        else
+        {
+            position = new Vector3(Random.Range(-3, 3), 0.5f, Random.Range(-3, 3));
+        }
+
+        PhotonNetwork.Instantiate(_tankPrefab.name, position, Quaternion.identity);
     }
 }
