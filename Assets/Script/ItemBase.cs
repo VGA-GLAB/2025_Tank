61240e87eda28ffa95bc97668fa34497d12448c3
tomo-1230 <satokawa1130tomoki2@gmail.com>
2025-10-12 15:51:54 +0900

    [add] アイテムベース

---
+++ Assets/Script/ItemBase.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using Photon.Pun;
+/// <summary>
+/// アイテムの基底クラス
+/// </summary>
+public abstract class ItemBase : MonoBehaviourPunCallbacks
+{
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player") && photonView.IsMine && PhotonNetwork.AutomaticallySyncScene)
+        {
+           photonView.RPC(nameof(HitAction), RpcTarget.All); // アイテム取得時の処理
+        }
+    }
+
+    /// <summary>
+    /// アイテム取得時の処理
+    /// </summary>
+    [PunRPC]
+    public abstract void HitAction();
+
+    /// <summary>
+    /// HitAction実行後にアイテムを消す
+    /// </summary>
+    public void Delete()
+    {
+        if(photonView.IsMine && PhotonNetwork.AutomaticallySyncScene)
+        {
+            PhotonNetwork.Destroy(this.gameObject);
+        }
+    }
+}
