fff7afd24723ef52701440435dfeaa2f832a236e
tomo-1230 <satokawa1130tomoki2@gmail.com>
2025-10-13 16:09:37 +0900

    [add] タイトルUI 　まだ動かない

---
+++ Assets/satokawa/TitleUIManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TitleUIManager : MonoBehaviour
+{
+    [SerializeField] private GameObject title;
+    [SerializeField] private GameObject roomJoin;
+    [SerializeField] private GameObject stageSelectSingle;
+    [SerializeField] private GameObject stageSelectMulti;
+    private void Start()
+    {
+        ChangeScreen(0);
+    }
+        
+    public void ChangeScreen(int number)
+    {
+        title.SetActive(number == 0);
+        roomJoin.SetActive(number == 1);
+        stageSelectSingle.SetActive(number == 2);
+        stageSelectMulti.SetActive(number == 3);
+    }
+}
