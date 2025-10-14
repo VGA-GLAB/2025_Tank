9b4cc19b759795d20d71422e65a009e34cc0760d
tomo-1230 <satokawa1130tomoki2@gmail.com>
2025-10-14 17:41:32 +0900

    [update] アイテム取得時にそのアイテムを取ったプレイヤーを取得できるように変更

--- Assets/Script/ItemBase.cs
+++ Assets/Script/ItemBase.cs
@@ -9,15 +9,16 @@ public abstract class ItemBase : MonoBehaviourPunCallbacks
     {
         if(other.CompareTag("Player") && photonView.IsMine && PhotonNetwork.AutomaticallySyncScene)
         {
-           photonView.RPC(nameof(HitAction), RpcTarget.All); // アイテム取得時の処理
+           photonView.RPC(nameof(HitAction), RpcTarget.All,other.gameObject); // アイテム取得時の処理
         }
     }
 
     /// <summary>
     /// アイテム取得時の処理
     /// </summary>
+    /// <param name="hitObject">アイテムを取得したオブジェクト</param>
     [PunRPC]
-    public abstract void HitAction();
+    public abstract void HitAction(GameObject hitObject);
 
     /// <summary>
     /// HitAction実行後にアイテムを消す
