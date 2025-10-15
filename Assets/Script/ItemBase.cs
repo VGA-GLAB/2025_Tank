1810b243dc066439e3cc3f1a67458c20a4df33c7
tomo-1230 <satokawa1130tomoki2@gmail.com>
2025-10-15 11:44:55 +0900

    [fix] PunRPCでの引数をGameObjectからViewIDに変更

--- Assets/Script/ItemBase.cs
+++ Assets/Script/ItemBase.cs
@@ -7,25 +7,32 @@ public abstract class ItemBase : MonoBehaviourPunCallbacks
 {
     public void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player") && photonView.IsMine && PhotonNetwork.AutomaticallySyncScene)
+        if (other.CompareTag("Player") && photonView.IsMine && PhotonNetwork.AutomaticallySyncScene)
         {
-           photonView.RPC(nameof(HitAction), RpcTarget.All,other.gameObject); // アイテム取得時の処理
+            if (other.TryGetComponent(out PhotonView view))
+            {
+                photonView.RPC(nameof(HitAction), RpcTarget.All, view.ViewID); // アイテム取得時の処理
+            }
+            else
+            {
+                Debug.LogError("PhotonViewがアタッチされていません");
+            }
         }
     }
 
     /// <summary>
     /// アイテム取得時の処理
     /// </summary>
-    /// <param name="hitObject">アイテムを取得したオブジェクト</param>
+    /// <param name="viewID">アイテムを取得したオブジェクトのviewID</param>
     [PunRPC]
-    public abstract void HitAction(GameObject hitObject);
+    public abstract void HitAction(int viewID);
 
     /// <summary>
     /// HitAction実行後にアイテムを消す
     /// </summary>
     public void Delete()
     {
-        if(photonView.IsMine && PhotonNetwork.AutomaticallySyncScene)
+        if (photonView.IsMine && PhotonNetwork.AutomaticallySyncScene)
         {
             PhotonNetwork.Destroy(this.gameObject);
         }
