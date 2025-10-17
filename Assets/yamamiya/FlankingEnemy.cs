89ad632fbeae9dce16031f9c241dd23753a2f9bc
YamamiyaShota <dragonsyouta10ys@gmail.com>
2025-10-17 14:09:52 +0900

    [add] FlankingEnemy作成

---
+++ Assets/yamamiya/FlankingEnemy.cs
@@ -0,0 +1,31 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+public class FlankingEnemy : EnemyBase
+{
+    private float _attackTimer;
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    public override void Move()
+    {
+        
+    }
+
+    public override void Attack()
+    {
+        _attackTimer += Time.deltaTime;
+        if(_attackTimer >= _bulletInterval)
+        {
+            GameObject newBullet = PhotonNetwork.Instantiate(_bulletPrefab.name, _muzzlePosition.position, Quaternion.identity);
+            newBullet.transform.forward = _muzzlePosition.forward;
+            if(newBullet.TryGetComponent<BulletControl>(out BulletControl component))
+            {
+                component._attack = _attack;
+            }
+            _attackTimer = 0f;
+        }
+    }
+}
