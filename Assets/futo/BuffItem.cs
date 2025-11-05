204b65cd93fca0a8c0f52954dc76bcaaaae5e40a
tomo-1230 <satokawa1130tomoki2@gmail.com>
2025-11-05 11:44:25 +0900

    [update]　BuffParticleを色変化に対応

--- Assets/futo/BuffItem.cs
+++ Assets/futo/BuffItem.cs
@@ -6,6 +6,7 @@ public class BuffItem : ItemBase
     [SerializeField] private  Buff _toBuff;
     [SerializeField] private float _buffAmount;
     [SerializeField] private ParticleSystem _buffParticle;
+    [SerializeField] private Color _particleColor;
 
     [PunRPC]
     public override void HitAction(int viewID)
@@ -22,6 +23,10 @@ public class BuffItem : ItemBase
             GameObject newParticle =  Instantiate(_buffParticle.gameObject,target.transform);
             newParticle.transform.localPosition = Vector3.zero;
             newParticle.transform.localRotation = Quaternion.Euler(-90f, 0f, 0f);
+            ParticleSystem.MainModule main = newParticle.GetComponent<ParticleSystem>().main;
+            ParticleSystem.MainModule sub = newParticle.transform.GetChild(0).GetComponent<ParticleSystem>().main;
+            main.startColor = _particleColor;
+            sub.startColor = _particleColor;
         }
         else
         {
