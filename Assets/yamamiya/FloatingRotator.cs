974c33107c2b7656301c7cae98a27c0f585ed53d
YamamiyaShota <153970961+YamamiyaShota@users.noreply.github.com>
2025-11-12 21:22:47 +0900

    [add] アイテムが上下に動きながら回転する処理の実装 [update] アイテムの見た目を差し替え

---
+++ Assets/yamamiya/FloatingRotator.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+public class FloatingRotator : MonoBehaviour
+{
+    [Header("上下移動設定")]
+    [SerializeField] private Vector3 _floatOffset = new Vector3(0f, 0.5f, 0f);
+    [SerializeField] private float _floatDuration = 1f;
+
+    [Header("回転設定")]
+    [SerializeField] private float _rotateDuration = 1f;
+    [SerializeField] private float _rotateDelay = 0.1f;
+    void Start()
+    {
+        DOTween.Sequence()
+            .Append(this.transform.DOMove(_floatOffset, _floatDuration)
+            .SetRelative(true)
+            .SetEase(Ease.InOutSine))
+            .SetLoops(-1, LoopType.Yoyo);
+
+        this.transform.DORotate(new Vector3(0f, 360f, 0f), _rotateDuration, RotateMode.FastBeyond360)
+            .SetEase(Ease.Linear)
+            .SetDelay(_rotateDelay)
+            .SetLoops(-1);
+    }
+
+    private void OnDestroy()
+    {
+        this.transform.DOKill();
+    }
+}
