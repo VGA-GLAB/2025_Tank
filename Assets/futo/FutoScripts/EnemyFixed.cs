aac5e77d385362303b6785cdc4f2f97211c6a27a
tomo-1230 <satokawa1130tomoki2@gmail.com>
2025-11-15 11:29:51 +0900

    [update]　スタンダードとパトロールを最新のモデルに置き換え

--- Assets/futo/FutoScripts/EnemyFixed.cs
+++ Assets/futo/FutoScripts/EnemyFixed.cs
@@ -41,7 +41,19 @@ public class EnemyFixed : EnemyBase
 
         if (_turret != null)
         {
-            _turret.transform.LookAt(_playerPosition);
+            //_turret.transform.LookAt(_playerPosition);
+            Vector3 localHit = _turret.transform.parent.InverseTransformPoint(_playerPosition);
+            Vector3 localDir = localHit - _turret.transform.localPosition;
+
+            // 水平距離と高さを求めてピッチ角を計算
+            float horizontalDistance = new Vector2(localDir.z, localDir.y).magnitude;
+            if (horizontalDistance > 0.001f)
+            {
+                float angleX = -Mathf.Atan2(localDir.y, localDir.z) * Mathf.Rad2Deg;
+
+                // X軸だけ回転
+                _turret.transform.localRotation = Quaternion.Euler(angleX, 0f, 0f);
+            }
         }
 
         if (!_agent.pathPending && _agent.remainingDistance <= _agent.stoppingDistance)
