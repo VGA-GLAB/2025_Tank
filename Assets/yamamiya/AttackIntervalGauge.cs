840f1456dc000944fc38140f887c228534888bf6
YamamiyaShota <153970961+YamamiyaShota@users.noreply.github.com>
2025-11-22 21:42:21 +0900

    [add] BulletIntervalを視覚化するAttackIntervalGaugeを作成。

---
+++ Assets/yamamiya/AttackIntervalGauge.cs
@@ -0,0 +1,20 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AttackIntervalGauge : MonoBehaviour
+{
+    [SerializeField] private Image _gauge;
+
+    /// <summary>
+    /// ゲージのFillAmountをアニメーションで変更する
+    /// </summary>
+    /// <param name="value"></param>
+    /// <param name="duration"></param>
+    public void AnimateFillAmount(float value, float duration)
+    {
+        // ゲージを一旦リセットしてからアニメーションさせる
+        _gauge.fillAmount = 0f;
+        _gauge.DOFillAmount(value, duration);
+    }
+}
